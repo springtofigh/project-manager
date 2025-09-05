140165f4d4717f30a2613ed252234bbb59803124
BT <bahartofigh@gmail.com>
2025-09-05 15:21:59 +0330

    Handling null initial values in the edit form to correctly display user data

--- components/module/ProfileData.js
+++ components/module/ProfileData.js
@@ -2,20 +2,20 @@
 function ProfileData({ data }) {
   return (
     <div className="profile-data">
-        <div>
-            <span>Name: </span>
-            <p>{data.name}</p>
-        </div>
-        <div>
-            <span>last Name: </span>
-            <p>{data.lastName}</p>
-        </div>
-        <div>
-            <span>Email: </span>
-            <p>{data.email}</p>
-        </div>
+      <div>
+        <span>Name: </span>
+        <p>{data?.name || "-"}</p>
+      </div>
+      <div>
+        <span>Last Name: </span>
+        <p>{data?.lastName || "-"}</p>
+      </div>
+      <div>
+        <span>Email: </span>
+        <p>{data?.email || "-"}</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProfileData;
